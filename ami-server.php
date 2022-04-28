e1d73e03487ba945822d2dec37f2e34a0455e5c1
AmilM7 <amil.murselovic@stu.ssst.edu.ba>
2022-04-28 14:04:58 +0200

    Add files via upload

---
+++ ami-server.php
@@ -0,0 +1,18 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+require('/home/amilCent/Documents/message-server.php');
+
+    require 'vendor/autoload.php';
+
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Messages()
+            )
+        ),
+        8080
+    );
+
+    $server->run();
